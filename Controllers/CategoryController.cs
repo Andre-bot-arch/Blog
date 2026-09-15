0f4907c8a7705d94ced69ea3cf177c579213afd0
Andre Moreira <andredev808@gmail.com>
2026-09-15 13:28:26 -0300

    conclusao de categoria completo

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 using Blog.Data;
+using Blog.Extensions;
 using Blog.Models;
 using Blog.ViewModels;
 using Microsoft.AspNetCore.Mvc;
@@ -41,6 +42,10 @@ namespace Blog.Controllers
         {
             try
             {
+
+                if (!ModelState.IsValid)
+                    return BadRequest(new ResultViewModel<Category>(ModelState.GetErrors()));
+
                 var category = new Category
                 {
                     Id = 0,
@@ -50,15 +55,15 @@ namespace Blog.Controllers
 
                 await context.Categories.AddAsync(category);
                 await context.SaveChangesAsync();
-                return Created($"v1/categories/{category.Id}", model);
+                return Created($"v1/categories/{category.Id}", new ResultViewModel<Category>(category));
             }
             catch (DbUpdateException ex)
             {
-                return StatusCode(500, "CTPT01 - Nao possivel incluir a categoria");
+                return StatusCode(500, new ResultViewModel<Category>("CTPT01 - Nao possivel incluir a categoria"));
             }
-            catch (Exception e)
+            catch
             {
-                return StatusCode(500, "CTPT02 - Falha interna do servidor");
+                return StatusCode(500, new ResultViewModel<Category>("CTPT02 - Falha interna do servidor"));
             }
 
         }
@@ -71,22 +76,22 @@ namespace Blog.Controllers
                 var category = await context.Categories.FirstOrDefaultAsync(x => x.Id == id);
 
                 if (category == null)
-                    return NotFound();
+                    return NotFound(new ResultViewModel<Category>("CTPUT01 - Categoria nao encontrada"));
 
                 category.Name = model.Name;
                 category.Slug = model.Slug;
                 context.Categories.Update(category);
                 await context.SaveChangesAsync();
 
-                return Created($"v1/categories/{category.Id}", model);
+                return Ok(new ResultViewModel<Category>(category));
             }
             catch (DbUpdateException ex)
             {
-                return StatusCode(500, "CTPUT01 - Nao possivel alterar a categoria");
+                return StatusCode(500, new ResultViewModel<Category>("CTPUT01 - Nao possivel alterar a categoria"));
             }
-            catch (Exception e)
+            catch
             {
-                return StatusCode(500, "CTPT02 - Falha interna do servidor");
+                return StatusCode(500, new ResultViewModel<Category>("CTPT02 - Falha interna do servidor"));
             }
         }
 
@@ -99,20 +104,20 @@ namespace Blog.Controllers
                 var category = await context.Categories.FirstOrDefaultAsync(x => x.Id == id);
 
                 if (category == null)
-                    return NotFound();
+                    return NotFound(new ResultViewModel<Category>("CTDEL01 - Categoria nao encontrada"));
 
                 context.Categories.Remove(category);
                 await context.SaveChangesAsync();
 
-                return Ok(category);
+                return Ok(new ResultViewModel<Category>(category));
             }
             catch (DbUpdateException ex)
             {
-                return StatusCode(500, "CTDEL01 - Nao possivel deletar a categoria");
+                return StatusCode(500, new ResultViewModel<Category>("CTDEL01 - Nao possivel deletar a categoria"));
             }
-            catch (Exception e)
+            catch
             {
-                return StatusCode(500, "CTDEL02 - Falha interna do servidor");
+                return StatusCode(500, new ResultViewModel<Category>("CTDEL02 - Falha interna do servidor"));
             }
         }
 
---
+++ Extensions/ModelStateExtensions.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Blog.Extensions;
+
+public static class ModelStateExtensions
+{
+    public static List<string> GetErrors(this ModelStateDictionary modelState)
+    {
+        var result = new List<string>();   
+        foreach (var item in modelState.Values)
+            result.AddRange(item.Errors.Select(e => e.ErrorMessage));
+            
+        return result;
+    }
+}
